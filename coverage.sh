799213e88568fcf725ce9a43470621284ffba8d4
massimo fioravanti <blalloscompany@gmail.com>
2019-07-12 17:49:10 +0200

    added coverage

--- coverage.sh
+++ coverage.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 cd cmake-build-coverage/
+cmake .. -DCMAKE_CXX_FLAGS="-fprofile-arcs -ftest-coverage"
 rm test.info
 rm filtered.info
 rm base.info
 rm total.info
 lcov --capture --initial --directory . --output-file ./base.info
-./test/runTest
+cmake --build . --target all
+cmake --build . --target test
 lcov --capture --directory . --output-file ./test.info
 lcov --add-tracefile ./base.info --add-tracefile ./test.info --output-file ./total.info
-lcov --remove ./total.info '/usr/*' '../g3log/*' 'test/*' '/vimExample/test/*' --output-file ./filtered.info
+lcov --remove ./total.info '/usr/*' '*/googletest/*' --output-file ./filtered.info
 genhtml ./filtered.info --output-directory ./Coverage
-vivaldi ./Coverage/index.html
